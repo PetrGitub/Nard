7d9a0f4c0858863813b0c81f954d5db6ab99312c
PetrFork <petr.visuals@email.cz>
2021-01-03 14:25:27 +0100

    MainController
    center of the game

---
+++ MainController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokus01_tvorba_sachovnice
+{
+    class MainController
+    {
+        public Board Nard;
+        public UserCommunication usCom;
+
+        public MainController()
+        {
+            Nard = new Board();
+            usCom = new UserCommunication();
+            this.Game();
+        }
+
+        public void Game()
+        {
+            while(true)
+            {
+                usCom.VypisBoard(Nard);
+                this.ProvedTah();
+                usCom.ConsoleClear();
+            }
+        }
+
+        // Provedeni tahu
+        public void ProvedTah()
+        {
+            int[] Odtud;
+            Odtud = usCom.ZadejTah();
+
+            int[] Sem;
+            Sem = usCom.ZadejTah();
+
+            Nard.hracideska[Sem[0], Sem[1]] = Nard.hracideska[Odtud[0], Odtud[1]];
+            Nard.hracideska[Odtud[0], Odtud[1]] = 0;
+        }
+    }
+}
