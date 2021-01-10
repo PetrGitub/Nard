44849191c5c6ab3bc095faa6f29a9a656406343e
PetrFork <petr.visuals@email.cz>
2021-01-10 21:39:19 +0100

    Board_adjusted

--- Board.cs
+++ Board.cs
@@ -6,8 +6,9 @@ namespace NARD_01
 {
     class Board
     {
-        private int m = 7, n = 8;
+        // private int m = 7, n = 8;
         public int[,] hracideska = new int[7, 8];
+
         public Board()
         {
             NaplnBoard();
