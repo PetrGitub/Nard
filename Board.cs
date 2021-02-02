0706efdd50b6a2f14cb1acd007ab5f93c7ed4905
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-02 21:07:49 +0100

    New added

--- Board.cs
+++ Board.cs
@@ -6,9 +6,8 @@ namespace NARD_01
 {
     class Board
     {
-        // private int m = 7, n = 8;
+        private int m = 7, n = 8;
         public int[,] hracideska = new int[7, 8];
-
         public Board()
         {
             NaplnBoard();
