3ae6e0eb0dbc06b48c5654a679bc472df61d91c1
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-04 20:39:50 +0100

    just adjusted
    pridano:     gameRules.NaplnBoard(Nard);

--- MainController.cs
+++ MainController.cs
@@ -6,14 +6,17 @@ namespace NARD_01
 {
     class MainController
     {
-        public Board Nard;
+        public Board Nard;                      // mám proměnnou Nard typu Board a ta ukazuje na tridu Board, kdyz vytvorim nejaky objekt te tridy, tak on bude umět vše, co je popsané ve třídě Board
         public UserCommunication usCom;
         public bool tahneBily = true;           // argument pro Volba( ........ ), r35
+        public Rules gameRules;
 
         public MainController()
         {
             Nard = new Board();
             usCom = new UserCommunication();
+            gameRules = new Rules();
+            gameRules.NaplnBoard(Nard);
             this.Game();
         }
 
@@ -32,7 +35,7 @@ namespace NARD_01
         public void ProvedTah()
         {
             int[][] Tah;
-            Tah = usCom.Volba(tahneBily);
+            Tah = usCom.Volba(tahneBily);       // provede se to, co je v podmince metody Volba, ale hodnota(true x false) pro tu metodu se vezme tady z r11
 
             int[] TahOdkud;
             TahOdkud = Tah[0];
