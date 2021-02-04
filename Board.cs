76234aa59f14db36bb118fde9269dabf30a2cf51
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-04 20:36:45 +0100

    just adjusted
    zustalo zde jen vytvoreni objektu "hracideska"

--- Board.cs
+++ Board.cs
@@ -10,31 +10,9 @@ namespace NARD_01
         public int[,] hracideska = new int[7, 8];
         public Board()
         {
-            NaplnBoard();
-        }
-
-        // Naplneni matice 
-        public void NaplnBoard()
-        {
-            for (int i = 0; i < this.hracideska.GetLength(0); i++)
-            {
-                for (int j = 0; j < this.hracideska.GetLength(1); j++)
-                {
-                    if (i <= 1)
-                    {
-                        this.hracideska[i, j] = 1;
-                    }
-                    else if (i >= 5)
-                    {
-                        this.hracideska[i, j] = 2;
-                    }
-                    else
-                    {
-                        this.hracideska[i, j] = 0;
-                    }
-                }
-            }
+           
         }
     }
 }
-// pokus
+
+
