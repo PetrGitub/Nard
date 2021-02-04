5641e003a80059b7aac623cb29566fea20e266d8
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-04 20:41:25 +0100

    just added
    byla sem presunuta metoda:      NaplnBoard(Board deska)

---
+++ Rules.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NARD_01
+{
+    class Rules
+    {
+        public Rules()
+        {
+           
+        }
+
+        // Naplneni matice 
+        public void NaplnBoard(Board deska)
+        {
+            for (int i = 0; i < deska.hracideska.GetLength(0); i++)       // this ....... ukazuje na konkretni hraciDesku, protoze jich muze existovat vic
+            {
+                for (int j = 0; j < deska.hracideska.GetLength(1); j++)
+                {
+                    if (i <= 1)
+                    {
+                        deska.hracideska[i, j] = 1;
+                    }
+                    else if (i >= 5)
+                    {
+                        deska.hracideska[i, j] = 2;
+                    }
+                    else
+                    {
+                        deska.hracideska[i, j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
