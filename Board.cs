e6a0c6d8104d7f07efecca8e0c3f6bb8d572b841
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-07 21:58:42 +0100

    just adjusted
    method VykonejTah()

--- Board.cs
+++ Board.cs
@@ -13,7 +13,11 @@ namespace NARD_01
         {
            
         }
+
+        public void VykonejTah(int[] nejakyPohyb)
+        {
+            this.hracideska[nejakyPohyb[3], nejakyPohyb[4]] = nejakyPohyb[5];   // na policko KAMtahnu vlozim info jakou figurkou {1  v   2  v  0}
+            this.hracideska[nejakyPohyb[0], nejakyPohyb[1]] = 0;                // tam odkud jsem odesel se vlozi 0 > tzn., neni tam zadna figurka
+        }
     }
 }
-
-
