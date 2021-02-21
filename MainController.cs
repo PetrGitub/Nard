602080c7b194d316e4e2c1b938528c2e796f2de4
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-21 21:12:53 +0100

    just added

--- MainController.cs
+++ MainController.cs
@@ -8,8 +8,10 @@ namespace NARD_01
     {
         public Board Nard;                      // mám proměnnou Nard typu Board a ta ukazuje na tridu Board, kdyz vytvorim nejaky objekt te tridy, tak on bude umět vše, co je popsané ve třídě Board
         public UserCommunication usCom;
-        public bool tahneBily = true;           // argument pro Volba( ........ ), r35
+        public int tahneBily = 1;           // argument pro Volba( ........ ), r35
         public Rules gameRules;
+
+        public string vypisTahu = "";           // vypíše provedený tah;  "" jsou tam proto, aby se vypsal (ytvoří se jen prázdné místo) hned při prvním průchodu, kdy ještě žádný tah není
 
         public MainController()
         {
@@ -25,9 +27,11 @@ namespace NARD_01
             while (true)
             {
                 usCom.VypisBoard(Nard);
+                usCom.VypisZpravu(vypisTahu);
                 this.ProvedTah();
                 usCom.ConsoleClear();
-                this.tahneBily = !tahneBily;    // !true   !false    prepinani bily-cerny
+                
+                this.tahneBily = -tahneBily;    // !1   !-1    prepinani bily-cerny
             }
         }
 
@@ -35,7 +39,7 @@ namespace NARD_01
         public void ProvedTah()
         {
             int[][] Tah;
-            Tah = usCom.Volba(tahneBily);       // provede se to, co je v podmince metody Volba, ale hodnota(true x false) pro tu metodu se vezme tady z r11
+            Tah = usCom.Volba(tahneBily == 1);       // provede se to, co je v podmince metody Volba, ale hodnota(1  V -1) pro tu metodu se vezme tady z r11
 
             int[] TahOdkud;
             TahOdkud = Tah[0];
@@ -44,12 +48,28 @@ namespace NARD_01
             TahKam = Tah[1];
 
 
-            int kamen_cil = Nard.hracideska[TahKam[0], TahKam[1]];            // vraci hodnotu (figurky: 1 v 2 v 0), ktera je na policku kam kracim > abych nevstoupil nekam, kde uz nekejaka fugyrka stoji
+            int kamen_cil = Nard.hracideska[TahKam[0], TahKam[1]];      // vraci hodnotu (figurky: 1 v 2 v 0), ktera je na policku kam kracim > abych nevstoupil nekam, kde uz nejaka figurka stoji
             int kamen = Nard.hracideska[TahOdkud[0], TahOdkud[1]];      // vraci hodnotu, ktera je na policku odkud jdu > abych vedel jaka bude hodnota (figurka) tam kam jdu
             int[] pohyb = { TahOdkud[0], TahOdkud[1], 0, TahKam[0], TahKam[1], kamen }; // A, 2, 0(=policko zustane prazdne] na A, 3, kamen(=jaka figurka se sem premistila: 1 v 2 v 0
-            
 
-            if(gameRules.KontrolaTahu(pohyb, tahneBily, kamen_cil))
+            List<int[]> platneTahy = gameRules.GenerujPlatneTahy(Nard, tahneBily);  // v Seznamu jsou platne tahy pro všechny figurky jedné (aktuální) barvy; před dalším tahem se vyčistí
+            foreach (int[] vypisPlatneTahy in platneTahy)
+            {
+                if (vypisPlatneTahy[0] == TahOdkud[0] && vypisPlatneTahy[1] == TahOdkud[1] && vypisPlatneTahy[4] == TahKam[0] && vypisPlatneTahy[5] == TahKam[1])
+                {                                                       // pokud jsou v platneTahy ty zadané tahy(jsou definované souřadnicemi), tak se vykonají(VykonejTah)
+                    Nard.VykonejTah(pohyb);
+                    vypisTahu = Convert.ToChar(TahOdkud[1] + 65) + (TahOdkud[0] + 1).ToString() + " → " + Convert.ToChar(TahKam[1] + 65) + (TahKam[0] + 1).ToString();
+                                                // tah se vypiše ...... souřadnice reprezentující písmena se převedou na písmena; čísla zůstanou jen se zvětší o +1, aby odpovídala hodnotám sloupců
+                    return;
+                }
+            }
+            usCom.ConsoleClear();
+            usCom.VypisBoard(Nard);
+            usCom.VypisZpravu("Error");
+            this.ProvedTah();
+
+
+            /*if (gameRules.KontrolaTahu(pohyb, tahneBily == 1, kamen_cil))
             {
                 Nard.VykonejTah(pohyb);
             }
@@ -59,7 +79,7 @@ namespace NARD_01
                 usCom.VypisBoard(Nard);
                 usCom.VypisZpravu("Error");
                 this.ProvedTah();
-            }
+            }*/
         }
     }
 }
