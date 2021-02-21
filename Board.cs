9c392e1a88ddf84f57c2f1bd515541c536c85c08
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-21 21:13:36 +0100

    just added

--- Board.cs
+++ Board.cs
@@ -19,5 +19,15 @@ namespace NARD_01
             this.hracideska[nejakyPohyb[3], nejakyPohyb[4]] = nejakyPohyb[5];   // na policko KAMtahnu vlozim info jakou figurkou {1  v   2  v  0}
             this.hracideska[nejakyPohyb[0], nejakyPohyb[1]] = 0;                // tam odkud jsem odesel se vlozi 0 > tzn., neni tam zadna figurka
         }
+
+        public int GetValue(int coordX, int coordY)                 // metoda vrátí hodnotu (figurku: 1  v  -1  v  0), která stojí na daných souřadnicích
+        {
+            return hracideska[coordX, coordY];
+        }
+
+        public void SetValue(int coordX, int coordY, int value)     // metoda vloží hodnotu (figurku: 1  v  -1  v  0), na dané souřadnice
+        {
+            hracideska[coordX, coordY] = value;
+        }
     }
 }
