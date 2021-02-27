05f5d40de2fdec5800b7ab865770c73d0df2e54f
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-02-27 17:24:53 +0100

    new adjustments

--- MainController.cs
+++ MainController.cs
@@ -11,7 +11,7 @@ namespace NARD_01
         public int tahneBily = 1;           // argument pro Volba( ........ ), r35
         public Rules gameRules;
 
-        public string vypisTahu = "";           // vypíše provedený tah;  "" jsou tam proto, aby se vypsal (ytvoří se jen prázdné místo) hned při prvním průchodu, kdy ještě žádný tah není
+        public string vypisTahu = "";           // vypíše provedený tah;  "" jsou tam proto, aby se vypsal (Vytvoří se jen prázdné místo) hned při prvním průchodu, kdy ještě žádný tah není
 
         public MainController()
         {
@@ -28,45 +28,63 @@ namespace NARD_01
             {
                 usCom.VypisBoard(Nard);
                 usCom.VypisZpravu(vypisTahu);
-                this.ProvedTah();
+                while( !UzivatelskeVolby() )
+                {
+                    usCom.VypisZpravu("  ------->   Error - Tah neni platny ");
+                }
                 usCom.ConsoleClear();
                 
                 this.tahneBily = -tahneBily;    // !1   !-1    prepinani bily-cerny
             }
         }
 
-        // Provedeni tahu
-        public void ProvedTah()
+        // Volby: move, help, generalHelp, ondo, redo, load, save
+        public bool UzivatelskeVolby()
         {
-            int[][] Tah;
-            Tah = usCom.Volba(tahneBily == 1);       // provede se to, co je v podmince metody Volba, ale hodnota(1  V -1) pro tu metodu se vezme tady z r11
+            int[] Tah;            // do promenne 'command' se vlozi "return Command.Move"(=táhni)  +  do promenne 'int[][] Tah' se vlozi TAH z "out int[][] Vlozeno_coords"
+            UserCommunication.Command command = usCom.Volba(tahneBily == 1, out Tah);       // provede se to, co je v podmince metody Volba, ale hodnota(1  V -1) pro tu metodu se vezme tady z r11
+            switch( command )
+            {
+                case UserCommunication.Command.Move:
+                    return ProvedTah(Tah);
 
-            int[] TahOdkud;
-            TahOdkud = Tah[0];
+                case UserCommunication.Command.GeneralHelp:
+                    //usCom.VypisZpravu("Pokusna verze");
+                    Console.WriteLine("hfvnhffi");
+                    return true;
+            }
+            return false;
+        }
 
-            int[] TahKam;
-            TahKam = Tah[1];
+        // Provedeni tahu
+        public bool ProvedTah(int[] Tah)    // tahy jsou na indexech -> Odkud[0 a 1], Kam[0 a 1]  =======> Tah(A2A3) = Tah[0123]
+        {
+            int[] TahOdkud = new int[2];    // pole se 2 prvky
+            TahOdkud[0] = Tah[0];
+            TahOdkud[1] = Tah[1];
+
+            int[] TahKam = new int[2];
+            TahKam[0] = Tah[2];
+            TahKam[1] = Tah[3];
 
 
             int kamen_cil = Nard.hracideska[TahKam[0], TahKam[1]];      // vraci hodnotu (figurky: 1 v 2 v 0), ktera je na policku kam kracim > abych nevstoupil nekam, kde uz nejaka figurka stoji
             int kamen = Nard.hracideska[TahOdkud[0], TahOdkud[1]];      // vraci hodnotu, ktera je na policku odkud jdu > abych vedel jaka bude hodnota (figurka) tam kam jdu
             int[] pohyb = { TahOdkud[0], TahOdkud[1], 0, TahKam[0], TahKam[1], kamen }; // A, 2, 0(=policko zustane prazdne] na A, 3, kamen(=jaka figurka se sem premistila: 1 v 2 v 0
 
+
             List<int[]> platneTahy = gameRules.GenerujPlatneTahy(Nard, tahneBily);  // v Seznamu jsou platne tahy pro všechny figurky jedné (aktuální) barvy; před dalším tahem se vyčistí
             foreach (int[] vypisPlatneTahy in platneTahy)
             {
                 if (vypisPlatneTahy[0] == TahOdkud[0] && vypisPlatneTahy[1] == TahOdkud[1] && vypisPlatneTahy[4] == TahKam[0] && vypisPlatneTahy[5] == TahKam[1])
-                {                                                       // pokud jsou v platneTahy ty zadané tahy(jsou definované souřadnicemi), tak se vykonají(VykonejTah)
+                {                                                       // pokud jsou v 'platneTahy' ty zadané tahy(jsou definované souřadnicemi), tak se vykonají(VykonejTah)
                     Nard.VykonejTah(pohyb);
                     vypisTahu = Convert.ToChar(TahOdkud[1] + 65) + (TahOdkud[0] + 1).ToString() + " → " + Convert.ToChar(TahKam[1] + 65) + (TahKam[0] + 1).ToString();
                                                 // tah se vypiše ...... souřadnice reprezentující písmena se převedou na písmena; čísla zůstanou jen se zvětší o +1, aby odpovídala hodnotám sloupců
-                    return;
+                    return true;                // = 'true' pokud se tah provede
                 }
             }
-            usCom.ConsoleClear();
-            usCom.VypisBoard(Nard);
-            usCom.VypisZpravu("Error");
-            this.ProvedTah();
+            return false;                       // = 'false' pokud se tah neprovede
         }
     }
 }
