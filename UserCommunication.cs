a20074a9f2f53efa57ed5d94b0ee6ff88d49ee86
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-10-18 17:34:18 +0200

    New new new

--- UserCommunication.cs
+++ UserCommunication.cs
@@ -16,22 +16,24 @@ namespace NARD_01
             Console.Clear();
         }
 
+
         // Vypsani matice
         public void VypisBoard(Board favouriteNard)
         {
             Console.Write("\n    A B C D E F G H\n");
 
-            for (int x = favouriteNard.hracideska.GetLength(0) - 1; x >= 0; x--)
+            for (int x = favouriteNard.hracideska.GetLength(0) - 1; x >= 0; x--)                // Ř Á D K Y
             {
-                Console.Write("{0,3} ", (x + 1).ToString());
-                for (int y = 0; y < favouriteNard.hracideska.GetLength(1); y++)
+                Console.Write("{0,3} ", (x + 1).ToString());                                                            // {0,3}  =>  {0}=vkládání čísel řádků -> (x + 1) = 7.........        3=odsazení prvního sloupce(7-1) od kraje
+                for (int y = 0; y < favouriteNard.hracideska.GetLength(1); y++)                 // S L O U P C E                   
                 {
-                    Console.Write("{0} ", IntToCharacter(favouriteNard.hracideska[x, y]));
+                    Console.Write("{0} ", IntToCharacter(favouriteNard.hracideska[x, y]));                              // metoda IntToCharacter prochází souřadnice na "hracideska", dostává: 1   -1    0    a vkládá -> x    o    prázdno
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
         }
+
 
         // Převod na string
         public string IntToCharacter(int value)
@@ -47,6 +49,7 @@ namespace NARD_01
             }
         }
 
+
         public enum Command                 // výčtový Typ
         {
             Move,
@@ -58,9 +61,10 @@ namespace NARD_01
             Save
         }
 
-        // Kdo je na tahu a zadavani souradnic   .......   texty (uvnitr kódu řeším konkrétní tahy - ZadejTah)
-        public Command Volba(bool tahneBily, out int[] Vlozeno_coords)        // argumenty     // pole intu 'Vlozeno_coords' = VYSTUPNI(= out) PARAMETR (tzn., vlozi se do nej souřadnice tahů)
-        {
+                                                                                                              // VÝSTUP směřuje do: MainController metoda UzivatelskeVolby()
+        // Kdo je na tahu a zadavani souradnic   .......   texty (uvnitr kódu řeším konkrétní tahy - ZadejTah)   VÝSTUP této metody: 1."return Command.Move;" (+souřadnice tahu se vloží do: "out int[] Vlozeno_coords"), 2.return "Command.GeneralHelp;" (+"null")
+        public Command Volba(bool tahneBily, out int[] Vlozeno_coords)      // argumenty     // pole intu 'Vlozeno_coords' => VYSTUPNI(= out) PARAMETR (tzn., vlozi se do nej souřadnice tahů => děje se to na r90)
+        {                                                                   
             if (tahneBily)  // podmínka
             {
                 Console.WriteLine("Bily na tahu");
@@ -70,16 +74,15 @@ namespace NARD_01
                 Console.WriteLine("Cerny na tahu");
             }
 
-
             int[] TahOdkud;                 // pole kam se zadaji souradnice ODKUD jde TAH
             int[] TahKam;                   // pole kam se zadaji souradnice KAM jde TAH
             Console.Write("   Zadej souradnice pole ODKUD chces tahout: ");
-            TahOdkud = this.ZadejTah();                                           // souradnice tahu ODKUD jsou v TahOdkud
+            TahOdkud = this.ZadejTah();                                         // souradnice tahu ODKUD jsou v TahOdkud
             Console.Write("   Zadej souradnice pole KAM chces tahout: ");
             TahKam = this.ZadejTah();                                           // souradnice tahu KAM jsou v TahKam
 
             Vlozeno_coords = null;
-            if(TahOdkud[0] == 91 || TahKam[0] == 91)
+            if(TahOdkud[0] == 91 || TahKam[0] == 91)                            // číslo 91 pochází z řádků 113-116 ZadejTah()
             {
                 return Command.GeneralHelp;
             }
@@ -88,6 +91,7 @@ namespace NARD_01
 
             return Command.Move;            // navratova hodnota je -----> Command
         }
+
 
         // Zadej tah
         public int[] ZadejTah()
@@ -100,9 +104,9 @@ namespace NARD_01
             {
                 chyba = -1;
                 
-                string zadani = Console.ReadLine();
+                string zadani = Console.ReadLine();     // ted mam v promenne "zadani" vložený tah (napr: A2)
 
-                if (zadani.Length > 0)
+                if (zadani.Length == 2 )
                 {
                     zadani = zadani.ToLower();          // z VELKYCH - male
 
@@ -113,11 +117,10 @@ namespace NARD_01
 
                     for (int i = 1; i >= 0; i--)
                     {
-                        b = (int)zadani[i];
-                        if (!(b < 49 || (55 < b && b < 97) || b > 104 || (zadani[0] > 48 && zadani[0] < 56)
-                            || (!((zadani[1] > 48 && zadani[1] < 56)))))
-                        {
-                            if (b >= 49 && b <= 55)
+                        b = (int)zadani[i];             // postupně čtu vložený tah (jdu od ZADU, tzn. jako první načtu ZNAK na indexu 1 a převedu na KÓD, potom index 0)
+                        if (!(b < 49 || (55 < b && b < 97) || b > 104 || (zadani[0] > 48 && zadani[0] < 56) || !(zadani[1] > 48 && zadani[1] < 56)))        // když to přepíšu v ASCII:  !(b<1 || (7<b && b<a) || b>h)
+                        {                                                                                                                                   // na indexu 0 se NESMÍ objevit čísla 1 - 7, na indexu 1 se MUSÍ objevit čísla 1 - 7
+                            if (b >= 49 && b <= 55)                             // 1 - 7
                             {
                                 tah_hrace[1 - i] = b - 49;
                             }
@@ -125,7 +128,7 @@ namespace NARD_01
                             //{
                             //tah[1 - i] = b - 65;
                             //}
-                            else if (b >= 97 && b <= 104)
+                            else if (b >= 97 && b <= 104)                       // a - h
                             {
                                 tah_hrace[1 - i] = b - 97;
                             }
@@ -133,14 +136,23 @@ namespace NARD_01
                         else
                         {
                             chyba += 1;
-                            Console.WriteLine("V zadani tahu se objevila nespravna kombinace. Opakujte zadani.\n");
+                            if (! (48 < zadani[1] && zadani[1] < 56) )                  // pokud mám na indexu 1 něco špatně zadané, vypíše se mi první zpráva, pokud mám špatné zadání na indexu 0, vypíše se druhá zpráva
+                            {
+                                Console.WriteLine("V zadani tahu se objevila nespravna   index1    kombinace. Opakujte zadani.\n");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("V zadani tahu se objevila nespravna   index0    kombinace. Opakujte zadani.\n");
+                                break;
+                            }
                         }
                     }
                 }
                 else
                 {
                     chyba += 1;
-                    Console.WriteLine("V zadani tahu se objevila nespravna kombinace. Opakujte zadani.\n");
+                    Console.WriteLine("V zadani tahu se objevila nespravna kombinace 10001. Opakujte zadani.\n");
                 }
             }
             Console.WriteLine();
