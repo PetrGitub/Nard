82da3fb2e0a2c8edc7ded8f2298014fab9938e02
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-10-18 17:36:47 +0200

    New new new

--- Rules.cs
+++ Rules.cs
@@ -13,10 +13,11 @@ namespace NARD_01
            
         }
 
-        // Naplneni matice 
+
+        // Naplneni matice   ----->    na indexy[i,j] se vloží: ( 1  -1   0 )  =====> a to se potom použije v UserCommunication při "VypisBoard"[x,y]
         public void NaplnBoard(Board deska)
         {
-            for (int i = 0; i < deska.hracideska.GetLength(0); i++)       // this ....... ukazuje na konkretni hraciDesku, protoze jich muze existovat vic
+            for (int i = 0; i < deska.hracideska.GetLength(0); i++)       // this ....... ukazuje na konkretni hraciDesku, protoze jich muze existovat vic             !!!!!   GetLength(0) --> sloupce   a   GetLength(1) --> řádky   !!!!!
             {
                 for (int j = 0; j < deska.hracideska.GetLength(1); j++)
                 {
@@ -28,15 +29,16 @@ namespace NARD_01
                     {
                         deska.hracideska[i, j] = -1;
                     }
-                    else
+                    /*else
                     {
                         deska.hracideska[i, j] = 0;
-                    }
+                    }*/
                 }
             }
         }
 
-        public List<int[]> GenerujPlatneTahy(Board deska, int kdoJeNaTahu)  // metoda vygeneruje všechny platné tahy pro aktuální barvu
+
+        public List<int[]> GenerujPlatneTahy(Board deska, int kdoJeNaTahu)  // metoda vygeneruje všechny platné tahy pro aktuální barvu (tzn. pro "x" nebo "o")
         {
             SeznamPlatnychTahu.Clear();
 
@@ -44,7 +46,7 @@ namespace NARD_01
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    if(deska.GetValue(i, j) == kdoJeNaTahu)     // pokud je na daných souřadnicích barva(1   v  -1), která je na tahu........
+                    if(deska.GetValue(i, j) == kdoJeNaTahu)     // pokud je na daných souřadnicích barva( 1  v -1 ), která je na tahu........
                     {
                         GenerujPlatneTahyProFigurku(i, j, deska, kdoJeNaTahu);      // .......vygeneruju do SeznamuPlatnýchTahů platné tahy figurky na těchto souřadnicích
                     }
@@ -53,20 +55,21 @@ namespace NARD_01
             return SeznamPlatnychTahu;
         }
 
+
         public void GenerujPlatneTahyProFigurku(int coordX, int coordY, Board deska, int kdoJeNaTahu)
         {
             int[,] directions =             
             {
-                { -1, 0 },  // doleva ===== index[0]
-                {  0, 1 },  //nahoru ===== index[1]
-                {  1, 0 },  //doprava ===== index[2]
-                {  0,-1 }   //dolu ===== index[3]
+                {  0,-1 },  // doleva ===== index[0]
+                {  1, 0 },  // nahoru ===== index[1]
+                {  0, 1 },  // doprava ===== index[2]
+                { -1, 0 }   // dolu ===== index[3]
             };
 
             for (int dir = 0; dir < 4; dir++)   //procházení 4 směrů ve smyčce ----->   smyčka nastaví hodnoty "x" a "y" ve 4 směrech = doleva, nahoru, doprava, dolu
             {                   // toX = x-ová souřadnice pole kam kráčím
-                int toX = coordX + directions[dir, 0];   // v proměnné "coordX" je x-ová souřadnice figurky;  dir=index směru;  0 a 1=indexy souřadnic v těch směrech
-                int toY = coordY + directions[dir, 1];   // v proměnné "coordY" je y-ová souřadnice figurky
+                int toX = coordX + directions[dir, 1];   // v proměnné "coordX" je x-ová souřadnice figurky;  dir=index směru (např: dir=0 -> {-1,0});  0 a 1=indexy souřadnic v těch směrech, tzn. => directions[{-1,0},0] ta druhá 0 říká, ......
+                int toY = coordY + directions[dir, 0];   // v proměnné "coordY" je y-ová souřadnice figurk-1,0y,0                                                                   .......... že si z té závorky vyberu čísli na indexu 0, což je -1
 
                 if ( !IsValidCoords(toX, toY) )
                     continue;   // continue = pokud je podmínka true = zastaví se proces a for se posune o +1 (pokračuje další iterace)
@@ -83,9 +86,10 @@ namespace NARD_01
             }
         }
 
-        public bool IsValidCoords(int souradniceX, int souradniceY)                 // metoda vrátí všechny souřadnice na šachovnici = ty validní
+
+        public bool IsValidCoords(int souradniceX, int souradniceY)                                 // metoda vrátí všechny souřadnice na šachovnici => ty validní     ->   jen ty souřadnice, na kterých se hraje
         {
-            return souradniceX >= 0 && souradniceX <= 6 && souradniceY >= 0 && souradniceY <= 7;
+            return souradniceX >= 0 && souradniceX <= 6 && souradniceY >= 0 && souradniceY <= 7;    // použitím této metody se vyloučí možnost dostat se mimo šachovnici
         }
     }
 }
