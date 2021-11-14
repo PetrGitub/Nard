34174d665360af91ad2b5b1125b091e6287c9b48
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-11-14 19:02:37 +0100

    New 14-11-2021

---
+++ ArtificialIntelligence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NARD_01
+{
+    class ArtificialIntelligence
+    {
+        private Random nahodnyVyber = new Random();
+
+        public ArtificialIntelligence()
+        {
+
+        }
+
+        public int[] vyberNahodnyTah( List<int[]> seznamTahu )
+        {
+            //int nahodnyTah = nahodnyVyber.Next(seznamTahu.Length);
+            //int vybranyTah = seznamTahu[nahodnyTah];
+            //return vybranyTah;
+            return seznamTahu[nahodnyVyber.Next(seznamTahu.Count)];
+        }
+    }
+}
