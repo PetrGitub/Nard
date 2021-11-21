197b95b9ee10dbdb416abba2911de405bbae8574
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-11-21 18:22:42 +0100

    New 21-11-2021

--- Board.cs
+++ Board.cs
@@ -7,35 +7,52 @@ namespace NARD_01
     class Board
     {
         //private int m = 7, n = 8;
-        public int[,] hracideska = new int[7, 8];
-
+        public int[,] hracideska = new int[ 7, 8 ];
+        public List<int[]> SeznamTahu = new List<int[]>();
+        
         public Board()
         {
            
         }
 
-        public void VykonejTah(int[] nejakyPohyb)
+
+        public void VykonejTah( int[] nejakyPohyb, bool tahZpet )
         {
-            for (int index = 0; index < nejakyPohyb.Length; index += 4)     // smyčka, která při každém průchodu zpracuje 4 prvky, provede změnu na desce a pak se posune o 4 prvky dál. Takhle projede celé pole, ať má délku 8, nebo 12 prvků...
-            {                                                               // |A|1|1|0| - na souřadnici A1 je figurka 1 a to se změní na 0 (figurka zmizí)     |A|2|0|1| - na souřadnici A2 není nic (0) a objejví se tam figurka 1
+            for (int index = 0; index < nejakyPohyb.Length; index += 4) // smyčka, která při každém průchodu zpracuje 4 prvky, provede změnu na desce a pak se posune o 4 prvky dál. Takhle projede celé pole, ať má délku 8, nebo 12 prvků...
+            {                                                           // |A|1|1|0| - na souřadnici A1 je figurka 1 a to se změní na 0 (figurka zmizí)     |A|2|0|1| - na souřadnici A2 není nic (0) a objejví se tam figurka 1
                 int x = nejakyPohyb[ index + 0 ];
                 int y = nejakyPohyb[ index + 1 ];
-                int novaHodnotaFigurky = nejakyPohyb[ index + 3 ];
+                int novaHodnotaFigurky;
+
+                if ( tahZpet )
+                {
+                    novaHodnotaFigurky = nejakyPohyb[ index + 2 ];        // index2 ukazuje, hodnotu figurky, která na políčko míří (asi 1 nebo -1)
+                }
+                else 
+                {
+                    novaHodnotaFigurky = nejakyPohyb[ index + 3 ];        // index3 ukazuje, co zůstane na políčku, ze kterého figurka odešla (většinou asi 0)
+                }
+
                 this.hracideska[ x, y ] = novaHodnotaFigurky;
             }
 
+            if( !tahZpet )
+            {
+                SeznamTahu.Add( nejakyPohyb );
+            }
+            
             //this.hracideska[nejakyPohyb[3], nejakyPohyb[4]] = nejakyPohyb[5];   // na policko KAMtahnu vlozim info jakou figurkou {1  v  -1  v  0}
             //this.hracideska[nejakyPohyb[0], nejakyPohyb[1]] = 0;                // tam odkud jsem odesel se vlozi 0 => tzn., neni tam zadna figurka
         }
 
-        public int GetValue(int coordX, int coordY)                 // metoda vrátí hodnotu (figurku: 1  v  -1  v  0), která stojí na daných souřadnicích
+        public int GetValue( int coordX, int coordY )                 // metoda vrátí hodnotu (figurku: 1  v  -1  v  0), která stojí na daných souřadnicích
         {
-               return hracideska[coordX, coordY];
+               return hracideska[ coordX, coordY ];
         }
          
-        public void SetValue(int coordX, int coordY, int value)     // metoda vloží hodnotu (figurku: 1  v  -1  v  0), na dané souřadnice
+        public void SetValue( int coordX, int coordY, int value )     // metoda vloží hodnotu (figurku: 1  v  -1  v  0), na dané souřadnice
         {
-            hracideska[coordX, coordY] = value;
+            hracideska[ coordX, coordY ] = value;
         }
     }
 }
