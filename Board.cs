c7fb8e297a69d1f5a8f26212db86189e49a2ac62
PetrGitub <75388693+PetrGitub@users.noreply.github.com>
2021-11-26 22:23:39 +0100

    New 26-11-2021

--- Board.cs
+++ Board.cs
@@ -16,7 +16,7 @@ namespace NARD_01
         }
 
 
-        public void VykonejTah( int[] nejakyPohyb, bool tahZpet )
+        public void VykonejTah( int[] nejakyPohyb, bool tahZpet, bool zapsatDoSeznamuTahu )
         {
             for (int index = 0; index < nejakyPohyb.Length; index += 4) // smyčka, která při každém průchodu zpracuje 4 prvky, provede změnu na desce a pak se posune o 4 prvky dál. Takhle projede celé pole, ať má délku 8, nebo 12 prvků...
             {                                                           // |A|1|1|0| - na souřadnici A1 je figurka 1 a to se změní na 0 (figurka zmizí)     |A|2|0|1| - na souřadnici A2 není nic (0) a objejví se tam figurka 1
@@ -36,7 +36,7 @@ namespace NARD_01
                 this.hracideska[ x, y ] = novaHodnotaFigurky;
             }
 
-            if( !tahZpet )
+            if( !tahZpet && zapsatDoSeznamuTahu )
             {
                 SeznamTahu.Add( nejakyPohyb );
             }
@@ -45,15 +45,18 @@ namespace NARD_01
             //this.hracideska[nejakyPohyb[0], nejakyPohyb[1]] = 0;                // tam odkud jsem odesel se vlozi 0 => tzn., neni tam zadna figurka
         }
 
+
         public int GetValue( int coordX, int coordY )                 // metoda vrátí hodnotu (figurku: 1  v  -1  v  0), která stojí na daných souřadnicích
         {
                return hracideska[ coordX, coordY ];
         }
          
+
         public void SetValue( int coordX, int coordY, int value )     // metoda vloží hodnotu (figurku: 1  v  -1  v  0), na dané souřadnice
         {
             hracideska[ coordX, coordY ] = value;
         }
+
 
         public void PocetFigur(out int pocetBilychFigur, out int pocetCernychFigur)
         {
